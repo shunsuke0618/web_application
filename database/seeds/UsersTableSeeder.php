10798be76d7f3579bc3a1403e7966cfb051f47c6
shunsuke inamura <shun0123.myys@gmail.com>
2020-12-21 01:12:36 +0900

    feat: シードデータ（ユーザー）の作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataSet = [
+        [
+            'name' => '山田太郎',
+            'email' => 'yamada@maol.com',
+            'password' => 'yamadayamada',   
+        ],
+        [
+            'name' => '山田花子',
+            'email' => 'hanako@mail.com',
+            'password' => 'hanakohanako',
+        ],   
+        [
+            'name' => '田中太郎',
+            'email' => 'tanaka@mail.com',
+            'password' => 'tanakatanaka',
+        ],  
+        [
+            'name' => '田中二郎',
+            'email' => 'jirou@mail.com',
+            'password' => 'jiroujirou',
+        ],  
+    ];    
+    foreach($dataSet as $data){
+        User::create($data);
+    }
+    }
+}
